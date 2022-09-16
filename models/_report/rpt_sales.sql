15260b99948351ce500cffdabd9bbe64bbcc9e01
pavan reddy <pavanreddy.talla@rrd.com>
2022-09-16 19:49:40 +0000

    updated_intermediate_rpt_models

---
+++ models/_report/rpt_sales.sql
@@ -0,0 +1,31 @@
+with customer_orders as (
+    select * from {{ ref('int_cust_orders') }}
+),
+customer_payments as (
+    select * from {{ ref('int_cust_payments') }}
+),
+customers as (
+    select * from {{ ref('stg_customers') }}
+),
+customers_sales_rpt as (
+
+    select
+        customers.customer_id,
+        customers.first_name,
+        customers.last_name,
+        customer_orders.first_order,
+        customer_orders.most_recent_order,
+        customer_orders.number_of_orders,
+        customer_payments.total_amount as customer_lifetime_value
+
+    from customers
+
+    left join customer_orders
+        on customers.customer_id = customer_orders.customer_id
+
+    left join customer_payments
+        on  customers.customer_id = customer_payments.customer_id
+
+)
+
+select * from customers_sales_rpt
---
+++ models/intermediate/int_cust_payments.sql
@@ -0,0 +1,26 @@
+with payments as (
+    select * from {{ ref('stg_payments') }}
+),
+
+orders as (
+    select * from {{ ref('stg_orders') }}
+),
+
+customer_payments as (
+
+    select
+        orders.customer_id,
+        sum(amount) as total_amount
+
+    from payments 
+
+    left join orders on
+         payments.orderid = orders.order_id
+
+    group by orders.customer_id
+)
+
+select 
+    *
+from
+    customer_payments;

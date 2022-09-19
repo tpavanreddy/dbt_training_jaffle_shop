737fb3c0008a5b3e9e13c1548c5bf56ae14216d2
pavan reddy <dakshgeo@gmail.com>
2022-09-19 14:11:18 +0000

    updated_timestamp_validation_macro

--- macros/timestamp_validation.sql
+++ macros/timestamp_validation.sql
@@ -1,4 +1,10 @@
+-- input==> timestamp_validation('20190503144721')	output==>2019-05-03T14:47:210
 {% macro timestamp_validation(timestamp_value) %}
-coalesce(try_to_timestamp(left({{timestamp_value}},4)||'-'||right(left({{timestamp_value}},6),2)||'-'||right(left({{timestamp_value}},8),2)||' '||right(left({{timestamp_value}},10),2)||':'||right(left({{timestamp_value}},12),2)||':'||right(left({{timestamp_value}},14),2)
-),'9999-12-31 00:00:00.000')
+    coalesce(try_to_timestamp(left({{timestamp_value}},4)||'-'||
+                              right(left({{timestamp_value}},6),2)||'-'||
+                              right(left({{timestamp_value}},8),2)||' '||
+                              right(left({{timestamp_value}},10),2)||':'||
+                              right(left({{timestamp_value}},12),2)||':'||
+                              right(left({{timestamp_value}},14),2)
+                              ),'9999-12-31 00:00:00.000')
 {% endmacro %}

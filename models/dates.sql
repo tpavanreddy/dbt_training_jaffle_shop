7ed76d4b4bdc56114b227bc93288ccebbf26e8e2
pavan reddy <dakshgeo@gmail.com>
2022-09-20 11:21:03 +0000

    updated_cln_models_dtypes_part

--- models/dates.sql
+++ models/dates.sql
@@ -1,7 +1,7 @@
 
-{{config(materialized='incremental',unique_key ='d_date')}}
+{{config(materialized='incremental', unique_key =['D_DATE_SK', 'D_DATE_ID'])}}
 
-select * from "SNOWFLAKE_SAMPLE_DATA"."TPCDS_SF10TCL"."DATE_DIM" where d_date <= current_date()+10
+select * from "SNOWFLAKE_SAMPLE_DATA"."TPCDS_SF10TCL"."DATE_DIM" where d_date <= current_date()+400
 
 {%if is_incremental() %}
 and d_date > (select max(d_date) from {{this}})

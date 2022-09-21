20565220042d7faa183505c941255b0f43ea9379
pavan reddy <dakshgeo@gmail.com>
2022-09-21 15:18:38 +0000

    cmt1

--- models/incremental_models_test/incremental_model_del_inst.sql
+++ models/incremental_models_test/incremental_model_del_inst.sql
@@ -1,4 +1,6 @@
-{{config(materialized='incremental', unique_key ='d_date', incremental_strategy='delete+insert')}}
+{{config(materialized='incremental'
+         , unique_key ='d_date'
+         , incremental_strategy='delete+insert')}}
 
 select *, current_timestamp as c_time from "SNOWFLAKE_SAMPLE_DATA"."TPCDS_SF10TCL"."DATE_DIM" where d_date <= current_date()+1500
 

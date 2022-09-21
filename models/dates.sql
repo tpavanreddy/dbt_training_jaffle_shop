e843ce9ba30d953dabb9267f2d411c04effefe91
varmagmv@gmail.com <varmagmv@gmail.com>
2022-09-21 05:58:22 +0000

    snapshots

--- models/dates.sql
+++ models/dates.sql
@@ -1,5 +1,5 @@
 
-{{config(materialized='incremental',unique_key ='d_date')}}
+{{config(materialized='incremental',unique_key ='d_date',incremental_strategy="merge")}}
 
 select * from "SNOWFLAKE_SAMPLE_DATA"."TPCDS_SF10TCL"."DATE_DIM" where d_date <= current_date()+10
 

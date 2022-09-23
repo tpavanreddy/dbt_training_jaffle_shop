a00e10ee96a8ae63ec4150c034f798fe6fb2133d
pavan reddy <dakshgeo@gmail.com>
2022-09-23 14:02:20 +0000

    inc_cmt

--- models/incremental_models_test/incremental_model_del_inst.sql
+++ models/incremental_models_test/incremental_model_del_inst.sql
@@ -1,8 +1,8 @@
 {{config(materialized='incremental'
-         , unique_key ='d_date'
+         , unique_key = 'ukey'
          , incremental_strategy='delete+insert')}}
 
-select *, current_timestamp as c_time from "SNOWFLAKE_SAMPLE_DATA"."TPCDS_SF10TCL"."DATE_DIM" where d_date <= current_date()+1500
+select concat(d_date, '-', d_date_sk) as ukey, *, current_timestamp as c_time from "SNOWFLAKE_SAMPLE_DATA"."TPCDS_SF10TCL"."DATE_DIM" where d_date <= current_date()+1500
 
 {%if is_incremental() %}
 and d_date > (select max(d_date) from {{this}})
